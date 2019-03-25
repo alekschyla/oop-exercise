7591901560068881b7cb80bcd7cf14c659f7ca9f
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-03-25 11:36:51 +0100

    Sum, average and median functions

---
+++ script.js
@@ -0,0 +1,21 @@
+const myArray = [9, 3, 12];
+
+function average (array) {
+    return sum(array) / array.length;
+}
+
+function sum(array) {
+    return array.reduce( (red, el) => red + el);
+}
+
+function median (array) {
+    array.sort(function(a,b) {return a - b;});
+    const half = Math.floor(array.length / 2);
+    if (array.length % 2 === 0) {
+        return array[half] + array[half - 1] / 2;
+    }
+    return array[half];
+}
+
+console.log(average(myArray));
+console.log(median(myArray));
