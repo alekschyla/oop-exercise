2b416f91530c50899abff61bc592fa9111a207f8
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-03-25 14:06:34 +0100

    Change app using ES5 constructor function and prototype

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-const myArray = [9, 3, 12];
+function CountApp(selector) {
+    this.container = document.querySelector(selector);
 
-function average (array) {
-    return sum(array) / array.length;
+    this.render();
 }
 
-function sum(array) {
-    return array.reduce( (red, el) => red + el);
-}
+CountApp.prototype.render = function () {
+    this.container.innerHTML = '';
 
-function median (array) {
-    array.sort(function(a,b) {return a - b;});
+    const input = document.createElement("input");
+    const button = document.createElement("button");
+    const paragraph = document.createElement("p");
+
+
+    button.innerText = "oblicz";
+    button.addEventListener("click", () => {
+        const object = this.returnObject(this.getInput(input.value));
+        this.addToParagraph(paragraph, object);
+    });
+
+    this.container.appendChild(input);
+    this.container.appendChild(button);
+    this.container.appendChild(paragraph);
+};
+
+CountApp.prototype.average = function(array) {
+    return this.sum(array) / array.length;
+};
+
+CountApp.prototype.sum = function (array) {
+    return array.reduce((red, el) => red + el);
+};
+
+CountApp.prototype.median = function (array) {
+    array.sort(function (a, b) {
+        return a - b;
+    });
 
     const half = Math.floor(array.length / 2);
 
@@ -18,24 +43,51 @@ function median (array) {
     }
 
     return array[half];
-}
+};
 
-function minNumber(array) {
+CountApp.prototype.minNumber = function (array) {
     return Math.min.apply(null, array);
-}
+};
 
-function maxNumber(array) {
-    return Math.max.apply(null, array);
-}
+CountApp.prototype.maxNumber = function (array) {
+   return Math.max.apply(null, array);
+};
 
-function returnObject(array) {
+CountApp.prototype.returnObject = function (array) {
     return {
-        average: average(array),
-        median: median(array),
-        minNumber: minNumber(array),
-        maxNumber: maxNumber(array),
+        average: this.average(array),
+        median: this.median(array),
+        minNumber: this.minNumber(array),
+        maxNumber: this.maxNumber(array),
     }
-}
+};
+
+CountApp.prototype.getInput = function (numbers) {
+    const numbersArray = numbers.split(' ');
+    const newArray = numbersArray.map(el => Number(el));
+    if (this.checkIfNumber(newArray)) {
+        return newArray;
+    }
+};
+
+CountApp.prototype.checkIfNumber = function (array) {
+    let isNumber = true;
+    array.forEach(el => {
+        if (isNaN(el)) {
+            isNumber = false;
+        }
+    });
+    return isNumber;
+};
+
+CountApp.prototype.addToParagraph = function (where, object) {
+    where.innerHTML = `
+    <strong>Average:</strong> ${object.average} |
+    <strong>Median:</strong> ${object.median} |
+    <strong>Minimum number:</strong> ${object.minNumber} |
+    <strong>Maximum number:</strong> ${object.maxNumber}
+    `
+};
 
 
-console.log(returnObject(myArray));
+const app = new CountApp('.container');
